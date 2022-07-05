1cdf352af5e97061da05f1b96ebd6dcb74cd5703
uripandada <uripandada@gmail.com>
2022-07-05 19:01:34 +0900

    commit(add model, controller, services)

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+Color orangeColor =Color(0xffDA873E);
+const Color primaryColor =Color(0xff1B3258);
+Color lightGray = Color(0xffF9F9F9);
+Color lightGreen = Color(0xff85D29F);
+Color orange = Color(0xffF39B6D);
+Color pink = Color(0xffFF706D);
+Color yellow = Color(0xffFFCB66);
+Color otherBlack = Color(0xFF222222) ;
+Color skyBlue = Color(0xff81CDEF);
+Color bottomBarColor = Color(0xff7c72ff);
+Color bottomSheetBackground = Color(0xFFF9F9F9);
+const Color selectLanguageGrey = Color(0xffF5F6FA);
+Color hintGrey = Color(0xff757F9D);
+const Color lightGrey = Color(0xfff4f4f4);
+const Color lightPurple = Color(0xff7c72ff);
+const Color white = Color(0xffffffff);
+const Color black = Color(0xff000000);
+Color darkBlue = Color(0xff142645);
+const Color darkGray = Color(0xff9696A1);
